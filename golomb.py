4a3e034f4165a3e104618af258fe728d00dd1b3e
Maria Anwar <mariaanwar2003@gmail.com>
2024-05-18 01:30:52 +0300

    Add files via upload

---
+++ golomb.py
@@ -0,0 +1,142 @@
+import Functions
+import math
+
+def golomb_cod(x,m):
+    c = int(math.ceil(math.log(m,2)))
+    remin = x % m
+    quo =int(math.floor(x / m))
+    #print "quo is",quo
+    #print "reminder",remin
+    #print "c",c
+    div = int(math.pow(2,c) - m)
+    #print "div",div
+    first = ""
+    for i in range(quo):
+        first = first + "1"
+    #print first
+
+    if (remin < div):
+        b = c - 1
+        a = "{0:0" + str(b) + "b}"
+        #print "1",a.format(remin)
+        bi = a.format(remin)
+        
+    elif (remin == div):
+        b = c 
+        a = "{0:0" + str(b) + "b}"
+        #print "1",a.format(remin)
+        bi = a.format(remin)
+
+    else:
+        b = c
+        a = "{0:0" + str(b) + "b}"
+        #print "2",a.format(remin+div)
+        bi = a.format(remin+div)
+
+    final = first + "0" +str(bi)
+    #print "final",final
+    return final
+
+
+def golomb(n,m):
+    n=n
+    m=m
+    golocode = golomb_cod(n,m)
+    bits_before=len(bin(n)[2:])
+    bits_after=len(golocode)
+    Compression_ratio=bits_before/bits_after
+
+    result = { 
+            "result": golocode,
+            "bits_before": bits_before,
+            "bits_after": bits_after,
+            "cr": Compression_ratio,
+            "avg_len": bits_after,
+            "efficiency": 0
+        }
+
+    return result
+
+
+x=golomb(42,6)
+print(x)
+
+#######################handel if user entered binary(tari2et el dr)####################################################################
+
+def binary_to_list(binary_string): ## binary to list of numbers
+    encoded_message = []
+
+    # start counting with the first charcter in the message
+    letter = binary_string[0]
+    count = 0
+    # iterate over each character in the message
+    for char in binary_string:
+        # if character is found at current index, count++
+        if(char == letter):
+            count = count + 1
+        # else (different character found at current index)
+        # append current count to encoded message
+        # then start counting the new character
+        else:
+            encoded_message.append(count)
+            letter = char
+            count = 1
+
+    encoded_message.append(count)        
+    # append count of the last character to encoded message
+    return encoded_message
+
+# Example usage
+# binary_string = "00000000100000000000100001111111111111111000000001111111010000000011"
+# result_list = binary_to_list(binary_string)
+# print(result_list)  
+
+ 
+def calculate_golomb_and_stats(binary_string):   #calculate golumb for list of numbers , di elfunction eli hanadiha lw user da5al binary 
+    m_list = []
+    result_list = binary_to_list(binary_string)
+    bits_before_total = 0
+    bits_after_total = 0
+    
+    for num in result_list:
+        m_list.append(round(math.sqrt(num)))
+    
+    golomb_results = []
+    for i in range(len(result_list)):
+        golocode = golomb(result_list[i], m_list[i])["result"]
+        #print(golocode)
+        bits_before_total = len(binary_string)  # Total bits before compression
+        bits_after = len(golocode)  # Total bits after compression
+        bits_after_total += bits_after
+        golomb_results.append(golocode)
+    
+    compression_ratio = bits_before_total / bits_after_total
+    
+    return {
+        "result": ''.join(golomb_results),
+        "bits_before": bits_before_total,
+        "bits_after": bits_after_total,
+        "cr": compression_ratio,
+        "efficiency": (bits_after_total/bits_before_total)*100
+    }
+
+# Example usage
+# binary_string = "00000000100000000000100001111111111111111000000001111111010000000011"
+# compression_stats = calculate_golomb_and_stats(binary_string)
+# print(compression_stats)
+
+##################################another if the user entered binary and clculate the decimal (tari2et elsokay)###################################
+# def binary_to_decimal(binary_string):
+#     """Converts a binary string to decimal."""
+#     return int(binary_string, 2)
+
+# def calculate_golomb_for_binary(binary_string):
+#     """Calculates Golomb encoding for a binary string."""
+#     decimal_value = binary_to_decimal(binary_string)
+#     m = round(math.sqrt(decimal_value))
+#     return golomb(decimal_value, m)
+
+# # Example usage
+# binary_string = "101010"
+# golomb_stats = calculate_golomb_for_binary(binary_string)
+# print(golomb_stats)
---
+++ huffman.py
@@ -0,0 +1,93 @@
+import Functions
+import heapq
+from collections import Counter, defaultdict
+
+class Node:
+    def __init__(self, char, freq):
+        self.char = char
+        self.freq = freq
+        self.left = None
+        self.right = None
+
+    def __lt__(self, other):
+        return self.freq < other.freq
+
+def build_huffman_tree(message):
+    frequency = Counter(message)
+    priority_queue = [Node(char, freq) for char, freq in frequency.items()]
+    heapq.heapify(priority_queue)
+
+    while len(priority_queue) > 1:
+        left = heapq.heappop(priority_queue)
+        right = heapq.heappop(priority_queue)
+        merged = Node(None, left.freq + right.freq)
+        merged.left = left
+        merged.right = right
+        heapq.heappush(priority_queue, merged)
+
+    return priority_queue[0]
+
+def build_huffman_mapping(node, code='', mapping=defaultdict(str)):
+    if node.char is not None:
+        mapping[node.char] = code
+    else:
+        build_huffman_mapping(node.left, code + '0', mapping)
+        build_huffman_mapping(node.right, code + '1', mapping)
+    return mapping
+
+def huffman_encode(message, mapping):
+    encoded_message = ''.join(mapping[char] for char in message)
+    return encoded_message
+
+def mapping_to_2d_array(mapping):
+    """
+    Convert mapping dictionary to a 2D array.
+
+    Args:
+        mapping (dict): Mapping of characters to Huffman codes.
+
+    Returns:
+        list: 2D array containing characters and their Huffman codes.
+    """
+    mapping_array = []
+    for char, code in mapping.items():
+        mapping_array.append([char, int(code)])
+    return mapping_array
+
+
+
+def compression_with_huffman(message):
+
+    tree = build_huffman_tree(message)
+    mapping = build_huffman_mapping(tree)
+    print(mapping)
+    array_mapping=mapping_to_2d_array(mapping)
+    print(array_mapping)
+
+
+    prob_table=Functions.calculate_probabilities(message)
+
+    avg_length=Functions.calculate_average_length(mapping,prob_table)
+    entropy=Functions.calculate_entropy(message)
+    efficiency=(entropy/avg_length)*100
+
+    encoded_message = huffman_encode(message, mapping)  # results
+    bits_before=Functions.calculate_bits_before(message)
+    bits_after = len(encoded_message)
+
+    Compression_ratio = bits_before / bits_after
+
+    results = {
+            "result": encoded_message,
+            "bits_before": bits_before,
+            "bits_after": bits_after,
+            "cr": Compression_ratio,
+            "entropy": entropy,
+            "avg_len": avg_length,
+            "efficiency": efficiency,
+            "prob_dict": prob_table
+        }
+
+    return results
+# v=compression_with_huffman("aaaabbccdf")
+# print(v)
